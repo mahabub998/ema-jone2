6e6e73282b231d9fca5fe164d94bec4368e39315
mahabub alam <mahabu998@gmail.com>
2021-02-27 21:36:23 +0600

    shop page codiing sone

--- src/components/Header/Shop/Shop.js
+++ src/components/Header/Shop/Shop.js
@@ -1,23 +1,37 @@
 
 import React, { useState } from 'react';
 import fakeData from '../../../fakeData';
-import Product from '../../Product/Product';
 import './Shop.css';
+import Product from '../../Product/Product';
+import Cart from '../../Cart/Cart';
 
 
 
 const Shop = () => {
-    const first10=fakeData.slice(0,15);
-   const[products,setProducts]= useState(first10)
+    const first10=fakeData.slice(0,10);
+   const[products,setProducts]= useState(first10);
+   const[cart,setCart]=useState([])
+
+const handelAddProduct =(product)=>{
+    
+    const newCart=[...cart,product];
+    setCart(newCart);
+}
+
     return (
         <div className="shop-container">
             <div className="product-container">
                 {
-                    products.map(pd=><Product product={pd}></Product>)
+                    products.map(pd=><Product
+                    handelAddProduct={handelAddProduct}
+                         product={pd}
+                         ></Product>)
+                
                 }
             </div>
            <div className="cart-container">
-                <h3>this is cart</h3>
+            <Cart cart={cart}></Cart>
+         
            </div>
             
         </div>
--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,12 +1,35 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee,faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import './Product.css'
+
 const Product = (props) => {
-    console.log(props.product.name)
-    return (
-        <div>
-          <h3>this is product</h3>  
-          <h4>{props.product.name}</h4>
-        </div>
-    );
+  // console.log(props)
+
+
+  const { img, name, seller, price, stock } = props.product;
+  return (
+    <div className="product">
+      <div>
+        <img src={img} alt="" />
+      </div>
+      <div>
+        <h4 className="product-name">{name}</h4>
+        <br></br>
+        <p><samll>by:{seller}</samll></p>
+        <p>${price}</p>
+        <br></br>
+        <p><small>Only {stock} left  in stock - order sonn</small> </p>
+        <button 
+        className="main-button" 
+        onClick={ ()=>props.handelAddProduct(props.product)}>
+          <FontAwesomeIcon icon={faShoppingCart} /> add to cart
+          </button>
+
+      </div>
+
+    </div>
+  );
 };
 
 export default Product;
